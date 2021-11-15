17141d1dfdbfb8eb8fa5799c90c2647fe9e66ae6
Cobalt0s <constantin.y.koval@gmail.com>
2021-11-14 21:26:13 -0800

    save

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sommelier',
-    version='0.19',
+    version='0.20',
     packages=find_packages(),
     license='MIT',
     description='Testing wrapper of Behave library',
