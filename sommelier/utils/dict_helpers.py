c11cff16e5e4f294097b40ba44a60ad4efc6ec4b
Cobalt0s <constantin.y.koval@gmail.com>
2022-01-08 10:13:17 -0800

    save

--- sommelier/utils/dict_helpers.py
+++ sommelier/utils/dict_helpers.py
@@ -7,7 +7,7 @@ class DictUtils:
 
     @staticmethod
     def is_assigned(obj, key):
-        return key in obj and obj[key] is not None
+        return key in obj and getattr(obj, key) is not None
 
     @staticmethod
     def declare(obj, key, value):
