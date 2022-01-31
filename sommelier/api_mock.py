906f9af0f4b96cb31430eb8453490d5474bbe049
Cobalt0s <constantin.y.koval@gmail.com>
2022-01-31 01:49:54 -0800

    save

--- sommelier/api_mock.py
+++ sommelier/api_mock.py
@@ -87,7 +87,7 @@ class APIMockManager(object):
 
     def is_satisfied(self):
         self.client.get('/mocks/services/unsatisfied')
-        data = get_json(self.context)
+        data = get_json(self.context).get("data")
         Judge(self.context).assumption(len(data.retriever_array()) == 0, 'some mocks are not satisfied')
 
     def remove_svc(self, svc):
